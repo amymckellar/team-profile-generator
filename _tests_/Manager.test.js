a02a85a38b0ba73b85c7f6b8fda3286cd1e64e5a
amymckellar <amy_mckellar@yahoo.com>
2023-03-12 18:58:31 -0500

    adding another test in manager file to get office number

--- _tests_/Manager.test.js
+++ _tests_/Manager.test.js
@@ -10,3 +10,9 @@ test("Return Manager", () => {
   const e = new Manager("Amy", 1, "amy_mckellar@yahoo.com", 100);
   expect(e.getRole()).toBe(testValue);
 });
+
+test("Get office number", () => {
+  const testValue = 100;
+  const e = new Manager("Amy", 1, "amy_mckellar@yahoo.com", testValue);
+  expect(e.getOfficeNumber()).toBe(testValue);
+});
